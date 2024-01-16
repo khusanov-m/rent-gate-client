60997cc3ccee603f4f2c8c3866d894c5143a69be
khusanov-m <khusanov.it.tech@gmail.com>
2024-01-16 21:06:03 +0500

    feat: test http backend hosted request

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,6 @@
+"use client";
+// REMOVE USE CLIENT
+
 import Footer from "@/components/Footer";
 import OurCustomers from "@/components/info/OurCustomers";
 import PerformanceSecurity from "@/components/info/PerformanceSecurity";
@@ -5,11 +8,38 @@ import SignUpCTA from "@/components/info/SignUpCTA";
 import WhyWe from "@/components/info/WhyWe";
 import HomeHeader from "@/components/root/HomeHeader";
 import HomeServices from "@/components/root/HomeServices";
+import { useQuery } from "@tanstack/react-query";
+
+{
+  /* REMOVE FROM */
+}
+export async function getPing() {
+  const d = await fetch("http://144.91.121.27:8000/api/v1/ping", {
+    method: "GET",
+  });
+  return await d.json();
+}
+{
+  /* REMOVE TO */
+}
 
 export default function Home() {
+  const { data, error, isLoading } = useQuery({
+    queryKey: ["ping"],
+    queryFn: getPing,
+  });
+
   return (
     <>
       <div className="flex flex-col min-h-screen">
+        {/* REMOVE FROM */}
+        {isLoading && <p>LOADING</p>}
+        {data && !isLoading && (
+          <p>
+            {data.status} {data.message}
+          </p>
+        )}
+        {/* REMOVE TO */}
         <HomeHeader />
         <HomeServices className="pt-24" />
         <WhyWe />
